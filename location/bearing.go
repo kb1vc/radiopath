957c29ed0d9b1deeea2cc36be3cf0b0ef673a300
Matt Reilly <kb1vc@kb1vc.org>
2016-06-05 08:45:54 -0400

    Added copyright headers

--- location/bearing.go
+++ location/bearing.go
@@ -1,3 +1,31 @@
+/*
+Copyright (c) 2012, Matthew H. Reilly (kb1vc)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 // Routines to calculate bearing and distance from one latlon to another
 package location
 
@@ -142,14 +170,13 @@ func (fr LatLon) OnPath(az float64, dist float64) LatLon {
 	/* *** AZIMUTHS IN RADIANS CLOCKWISE FROM NORTH */
 	/* *** GEODESIC DISTANCE S ASSUMED IN UNITS OF SEMI-MAJOR AXIS A */
 
-	/* *** PROGRAMMED FOR CDC-6600 BY LCDR L.PFEIFER NGS ROCKVILLE MD 20FEB75 
-*/
+	/* *** PROGRAMMED FOR CDC-6600 BY LCDR L.PFEIFER NGS ROCKVILLE MD 20FEB75 */
 	/* *** MODIFIED FOR SYSTEM 360 BY JOHN G GERGEN NGS ROCKVILLE MD 750608 */
 	/* *** HACKED TO HELL AND GONE BY F2C (July-17-96 version) and by 
            *** Matt Reilly (KB1VC) to make it fit with the dem-gridlib routines.
            *** Feb 24, 1997. */
 
-	/* translated to "go" in June of 2016 by Matt Reilly (kb1vc) */
+	/* *** translated to "go" in June of 2016 by Matt Reilly (kb1vc) */
 
 	var sy, cy, cz float64
 	var e, c, d, x, y float64
